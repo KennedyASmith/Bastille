e6ba8bc1f97ee501e23be2415a82979f8c3b912a
Andrew <AndrewOMagnuson@gmail.com>
2022-12-30 18:46:20 -0600

    Added alot more metals into config

--- src/main/java/com/kennedysmithjava/prisoncore/crafting/Rarity.java
+++ src/main/java/com/kennedysmithjava/prisoncore/crafting/Rarity.java
@@ -1,19 +1,31 @@
 package com.kennedysmithjava.prisoncore.crafting;
 
-public enum Rarity {
-    COMMON("&fCommon"),
-    UNCOMMON("&aUnCommon"),
-    RARE("&9Rare"),
-    EPIC("&5Epic"),
-    LEGENDARY("&6Legendary"),
+import lombok.Getter;
 
-    MYTHIC("&b&lMYTHIC");
+public enum Rarity {
+    COMMON("&fCommon", 1),
+    UNCOMMON("&aUnCommon", 2),
+    RARE("&9Rare", 3),
+    EPIC("&5Epic", 4),
+    LEGENDARY("&6Legendary", 5),
+
+    MYTHIC("&b&lMYTHIC", 10);
 
 
     final String displayName;
+    @Getter
+    final int rarity;
 
-    Rarity(String displayName) {
+    Rarity(String displayName, int rarity) {
         this.displayName = displayName;
+        this.rarity = rarity;
+    }
+
+    public static Rarity getFromRarityInt(int rarity) {
+        for (Rarity value : Rarity.values()) {
+            if (value.getRarity() == rarity) return value;
+        }
+        return Rarity.COMMON;
     }
 
     @Override
--- src/main/java/com/kennedysmithjava/prisoncore/crafting/objects/type/MetalType.java
+++ src/main/java/com/kennedysmithjava/prisoncore/crafting/objects/type/MetalType.java
@@ -18,13 +18,31 @@ public enum MetalType {
     SCUTE(Material.SCUTE, "&aScute", 2, 4),
     CLAY_BALL(Material.CLAY_BALL, "&7Clay Ball", 2, 2),
 
-    FLINT(Material.FLINT, "&8Flint", 3, 1);
+    // Rare
+    FLINT(Material.FLINT, "&8Flint", 3, 1),
+    GOLD_INGOT(Material.GOLD_INGOT, "&e&lGold Ingot", 3, 3),
+    SLIME_BALL(Material.SLIME_BALL, "&aSlime Ball", 3, 1),
 
+    // Epic
+    LAPIS_LAZULI(Material.LAPIS_LAZULI, "&9Lapis Lazuli", 4, 2),
+    DIAMOND(Material.DIAMOND, "&bDiamond", 4, 5),
+    NETHERITE_INGOT(Material.NETHERITE_INGOT, "&8Netherite Ingot", 4, 5),
+
+    // Legendary
+
+    QUARTZ(Material.QUARTZ, "&f&lQuartz", 5, 5),
+    EMERALD(Material.EMERALD, "&a&lEmerald", 5, 4),
+
+    // Mythic
+    HEART_OF_THE_SEA(Material.HEART_OF_THE_SEA, "&e&lHeart Of The Sea", 10, 3),
+    AMETHYST(Material.AMETHYST_SHARD, "&d&lAmethyst Shard", 3, 10, Rarity.MYTHIC);
     final String displayName;
     final Material material;
     final int hardness;
     final int rarity;
     final List<String> lore;
+
+    final Rarity rarityClass;
 
 
     MetalType(Material material, String displayName, int rarity, int hardness) {
@@ -41,6 +59,7 @@ public enum MetalType {
                 "",
                 "%rarityDisplay% &7material used for crafting &etools&7!"
         );
+        rarityClass = Rarity.getFromRarityInt(rarity);
     }
 
     MetalType(Material material, String displayName, int rarity, int hardness, List<String> customLore) {
@@ -49,6 +68,32 @@ public enum MetalType {
         this.rarity = rarity;
         this.material = material;
         this.lore = customLore;
+        rarityClass = Rarity.getFromRarityInt(rarity);
+    }
+
+    MetalType(Material material, String displayName, int rarity, int hardness, Rarity rarityClass) {
+        this.displayName = displayName;
+        this.hardness = hardness;
+        this.rarity = rarity;
+        this.material = material;
+        this.rarityClass = rarityClass;
+        lore = MUtil.list(
+                "&7Crafting Material",
+                " &r",
+                "&7Hardness: &f%hardness%",
+                "&7Rarity: &f%rarity%",
+                "",
+                "%rarityDisplay% &7material used for crafting &etools&7!"
+        );
+    }
+
+    MetalType(Material material, String displayName, int rarity, int hardness, List<String> customLore, Rarity rarityClass) {
+        this.displayName = displayName;
+        this.hardness = hardness;
+        this.rarity = rarity;
+        this.material = material;
+        this.lore = customLore;
+        this.rarityClass = rarityClass;
     }
 
 }
